ccf4ffc1d7176571cede22afecdb3dfa644fe4c9
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2020-12-14 16:47:07 +0100

    add currentDate

--- js/todo.js
+++ js/todo.js
@@ -1,2 +1,7 @@
 'use strict';
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currendtDate = new Date();
+(() => document.querySelector('.date').innerHTML
+    = `${days[currendtDate.getDay()]}<br>
+    ${currendtDate.getMonth()}-${currendtDate.getDate()}-${currendtDate.getFullYear()}`)();
 
